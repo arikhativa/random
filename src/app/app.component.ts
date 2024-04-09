828a9faf9233a056dc1065021d3f251a07834c5b
Arik Hativa <arikhativa@gmail.com>
2024-04-09 19:42:34 +0300

    CoolHover6Blocks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, viewChild, ViewChild, ViewChildren } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'del';
+    @ViewChild('cards') cards: ElementRef<HTMLElement> | undefined;
+    title = 'del';
+    ngAfterViewInit() {
+        if (!this.cards) return
+        this.cards.nativeElement.onmousemove = this.handleMouseMove;
+    }
+
+    handleMouseMove(e: MouseEvent) {
+        const cards = Array.from(document.getElementsByClassName('card')) as HTMLElement[];
+        for (const card of cards) {
+            const rect = card.getBoundingClientRect();
+            const x = e.clientX - rect.left;
+            const y = e.clientY - rect.top;
+
+            card.style.setProperty('--mouse-x', `${x}px`);
+            card.style.setProperty('--mouse-y', `${y}px`);
+        }
+    }
 }
